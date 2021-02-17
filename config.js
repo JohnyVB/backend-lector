a57d68cfcc78336e4947b12169769f0b6b4837e2
Johny Villegas <55726826+JohnyVB@users.noreply.github.com>
2021-02-17 16:28:11 -0500

    Modificacion en config.js

--- config.js
+++ config.js
@@ -1,20 +1,10 @@
 
 module.exports = {
-    TOKEN_SECRET: process.env.TOKEN_SECRET || "tokenultrasecreto",
-    PORT: process.env.PORT || 3900,
-    application: {
-        cors: {
-            server: [
-                {
-                    origin: "*", 
-                    credentials: true
-                }
-            ]
-        }
-    },
-    EMAIL: process.env.EMAIL || "readerbookappweb@gmail.com",
-    PASSEMAIL: process.env.PASSEMAIL || "Dragter22joh54.",
-    BDTEST: process.env.BDTEST || "mongodb://localhost:27017/api-rest-lector",
-    BDPROD: process.env.BDPROD || "mongodb+srv://root:root3517@cluster0.p9hkj.mongodb.net/api-rest-lector?retryWrites=true&w=majority",
-    BDPRODTEST: process.env.BDPRODTEST || "mongodb+srv://root:root3517@cluster0.p9hkj.mongodb.net/api-rest-lector-test?retryWrites=true&w=majority"
+    TOKEN_SECRET: process.env.TOKEN_SECRET,
+    PORT: process.env.PORT,
+    EMAIL: process.env.EMAIL,
+    PASSEMAIL: process.env.PASSEMAIL,
+    BDTEST: process.env.BDTEST,
+    BDPROD: process.env.BDPROD,
+    BDPRODTEST: process.env.BDPRODTEST
 }
